af5058996b57e332c04258fa9b5f9c108fa469db
mateusz.para <mateusz.para@HK-01>
2023-10-29 13:30:44 +0100

    Add project files.

---
+++ LAB3/Controllers/DaneController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB3.Models;
+
+namespace LAB3.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane) 
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
